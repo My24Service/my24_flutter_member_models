9d9c5d22c2a2ad4541ab6546a5d8418c4bf8e46b
Richard Jansen <richard@pedroja.tech>
2024-02-10 11:48:55 +0100

    first commit

---
+++ lib/private/api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:my24_flutter_core/api/base_crud.dart';
+import 'models.dart';
+
+class PrivateMemberApi extends BaseCrud<PrivateMember, PrivateMembers> {
+  @override
+  set basePath(String path) {
+    basePath = "/member/member";
+  }
+
+  @override
+  PrivateMember fromJsonDetail(Map<String, dynamic>? parsedJson) {
+    return PrivateMember.fromJson(parsedJson!);
+  }
+
+  @override
+  PrivateMembers fromJsonList(Map<String, dynamic>? parsedJson) {
+    return PrivateMembers.fromJson(parsedJson!);
+  }
+
+  Future<Map<String, dynamic>?> fetchSettings() async {
+    final String response = await super.getListResponseBody(
+        basePathAddition: 'get_my_settings/');
+
+    return json.decode(response);
+  }
+}
